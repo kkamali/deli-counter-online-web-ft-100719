cdcebccd704b9a2045485cd5bb1447ed07e1f4dd
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 17:07:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(people)
   if people.length == 0
     puts "The line is currently empty."
   else 
-    print "The line is currently: "
-    people.each_with_index(|person| print "#{index}. #{person}" )
+    print "The line is currently:"
+    people.each_with_index(|person| print " #{index}. #{person}" )
   end
 end
